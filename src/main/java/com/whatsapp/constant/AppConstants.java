617f11e868a4257af07ce39328bed041c6f47d00
Mahendra <53494025+mahendra296@users.noreply.github.com>
2024-09-09 20:06:59 +0530

    Delete src directory

--- src/main/java/com/whatsapp/constant/AppConstants.java
+++
@@ -1,9 +0,0 @@
-package com.whatsapp.constant;
-
-public class AppConstants {
-    public static final String START_EVENT = "START";
-    public static final String CONTINUE_EVENT = "CONTINUE";
-    public static final String SESSION_TIMEOUT_TERMINATE_EVENT = "TERMINATE:SessionTimeOut";
-    public static final String USER_INITIATED_TERMINATE_EVENT = "TERMINATE:UserInitiated";
-    public static final String END_OF_MENU_TERMINATE_EVENT = "TERMINATE:EndOfMenu";
-}
